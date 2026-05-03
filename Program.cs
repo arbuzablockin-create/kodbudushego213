02640f3610d79a81115d08bee4cdbb93181ee5b1
arbuzablockin-create <arbuzablockin@gmail.com>
2026-05-04 01:17:21 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codbudus12
+{
+    public class TaskItem
+    {
+        public string Title { get; set; }
+    
+        static void Main(string[] args)
+        {
+        }
+    }
+}
